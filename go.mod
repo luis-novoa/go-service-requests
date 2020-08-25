2f0f7e01c762847d9e8454da3fe97623a4e22df3
Luis Novoa <luis.matte.novoa@gmail.com>
2020-08-25 17:34:50 -0400

    Implements endpoint for graphiQL

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/luis-novoa/go-service-requests
 
 go 1.15
 
-require github.com/graphql-go/graphql v0.7.9 // indirect
+require (
+	github.com/friendsofgo/graphiql v0.2.2 // indirect
+	github.com/graphql-go/graphql v0.7.9 // indirect
+)
--- main.go
+++ main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"encoding/json"
 	"github.com/graphql-go/graphql"
+	"github.com/friendsofgo/graphiql"
 )
 
 func main() {
-	graphQLHTTP := func(w http.ResponseWriter, req *http.Request) {
-		query := executeQuery(r.URL.Query().Get("query"), schema)
+	graphQLHTTP := func(w http.ResponseWriter, r *http.Request) {
+		query := graphql.Do(graphql.Params{
+			Schema: schema,
+			RequestString: r.URL.Query().Get("query")
+		})
+
+		if len(query.Errors) > 0 {
+			fmt.Printf("errors: %v", query.Errors)
+		}
+
 		json.NewEncoder(w).Encode(query)
 	}
 
+	graphiQLInterface, err := graphiql.NewGraphiqlHandler("/")
+	if err != nil { panic(err) }
+
 	http.HandleFunc("/", graphQLHTTP)
-	log.Println("Server running on localhost:3000")
+	http.Handle("/interface", graphiQLInterface)
+	log.Println("Server running. Make HTTP requests directly to localhost:3000 or access localhost:3000/interface to interact through GraphiQL.")
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
